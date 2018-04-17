3f870ba7a2d71850af850608daef4ff917489ac5
lsm <1442933609@qq.com>
2018-04-17 21:47:31 +0800

    4.17 lsm

--- tp/Apps/Runtime/Cache/Home/400c995c330f1f6ecac6b9102849eb7e.php
+++ tp/Apps/Runtime/Cache/Home/400c995c330f1f6ecac6b9102849eb7e.php
@@ -3,14 +3,16 @@
 	<head>
 		<meta charset="UTF-8">
 		<title></title>
-		<script src="Public/jquery-3.3.1.js"></script>
 		<link rel="stylesheet" type="text/css" href="Public/bootstrap-3.3.7/css/bootstrap.min.css"/>
+		<script src="Public/jquery-3.3.1.js"></script>
 		<script src="Public/bootstrap-3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<div class="nav"></div>
+		<div class="sildeshow"></div>
 	</body>
 	<script type="text/javascript">
+		$(".sildeshow").load("Apps/Home/View/slideshow.html");
 		$(".nav").load("Apps/Home/View/nav.html");
 	</script>
 </html>
